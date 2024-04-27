034328b6c6840e5f3ffa877eb0f2dfb1c9e9fee9
Gityosan <ymo6190@docomo.ne.jp>
2024-04-27 12:44:41 +0900

    [fix] reset files

--- middleware/index.global.ts
+++ middleware/index.global.ts
@@ -1,6 +1,1 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-  if (/prohibited/.test(to.path)) {
-    alert('Access to this page is prohibited. Redirecting to the about page.')
-    return navigateTo('/about')
-  }
-})
+export default defineNuxtRouteMiddleware(async (to) => {})
--- pages/prohibited.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-useHead({ title: 'prohibited' })
-</script>
-<template>
-  <p>This page is prohibited.</p>
-</template>
